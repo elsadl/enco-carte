17eac8ff4b9f46e7d8e2e695d86c245622c775c6
Cerdic <cedric@yterium.com>
2021-10-12 16:40:54 +0200

    Configuration pour faire marcher la carte dans tous les cas

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+        <?php $baseurl = dirname($_SERVER['SCRIPT_NAME']).'/'; ?>
+
+        <title>The Profit with Healthcare</title>
+
+        <link rel='icon' type='image/png' href='<?=$baseurl ?>favicon.png'>
+        <link rel='stylesheet' href='<?=$baseurl ?>global.css'>
+        <link rel='stylesheet' href='<?=$baseurl ?>build/bundle.css'>
+
+        <script defer src='<?=$baseurl ?>build/bundle.js'></script>
+</head>
+
+<body>
+</body>
+</html>
